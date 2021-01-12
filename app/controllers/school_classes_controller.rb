33f554858d6dd0109e47ae96456ed92d49483761
Doode1524 <Doode1524@gmail.com>
2021-01-12 00:07:47 -0500

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -11,22 +11,27 @@ class SchoolClassesController < ApplicationController
     end
 
     def show
-
+        find_school_class
     end
 
     def edit
-
+        find_school_class
     end
 
     def update
-
+        find_school_class
+        @school_class.update(school_class_params)
+        redirect_to school_class_path(@school_class)
     end
 
     private
 
-def school_class_params
-  params.require(:school_class).permit(:title, :room_number)  
-end 
+    def school_class_params
+        params.require(:school_class).permit(:title, :room_number)  
+    end
 
+    def find_school_class
+        @school_class = SchoolClass.find_by_id(params[:id])
+    end
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,21 +11,28 @@ class StudentsController < ApplicationController
     end
 
     def show
-        @student = Student.find(params[:id])
+        find_student
     end
 
     def edit
-
+        find_student
     end
 
     def update
-
+        find_student
+        @student.update(student_params)
+        redirect_to student_path(@student)
     end
 
     private
 
     def student_params
       params.require(:student).permit(:first_name, :last_name)
+    end
+
+    def find_student
+        @student = Student.find_by_id(params[:id])
+
     end
 
 end
