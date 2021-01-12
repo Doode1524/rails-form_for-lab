48d04d64f020637c3513a7f98885c8e1ec2944cb
Doode1524 <Doode1524@gmail.com>
2021-01-11 23:28:51 -0500

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,32 @@
+class SchoolClassesController < ApplicationController
+
+    def new
+        @school_class = SchoolClass.new
+    end
+
+    def create
+        @school_class = SchoolClass.new(school_class_params)
+        @school_class.save
+        redirect_to school_class_path(@school_class) 
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    private
+
+def school_class_params
+  params.require(:school_class).permit(:title, :room_number)  
+end 
+
+
+end
